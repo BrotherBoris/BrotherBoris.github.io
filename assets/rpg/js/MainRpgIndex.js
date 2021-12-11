0ebf554a2bc9ed311329bac7562aea3f68c4d8c6
BrotherBoris <joaoantoniofer@gmail.com>
2021-12-11 00:22:35 -0300

    Discovered js doesnt have overloading  11-12-21

--- assets/rpg/js/MainRpgIndex.js
+++ assets/rpg/js/MainRpgIndex.js
@@ -14,18 +14,39 @@ function NameStarter() {
         inputName = "Nameless_one";
     }
     userName = inputName;
-    CreateMainMenu(userName);
-    
+    CreateMainMenu(userName);    
+}
+
+
+function Prina(x,y,z) {
+    if (x == "undefined" && y == "undefined" && z == "undefined"){
+        console.log("worked");
+    }
+    if(x!="undefined")
+        console.log(x);
+    if (y != "undefined")
+        console.log(y);
+    if (z != "undefined")
+        console.log(z);
 }
 
 function CreateMainMenu(user){
-    CreateMenuOnTheFly("MainMenu", "Main Menu", 4);
+    CreateMenuOnTheFly("MainMenu", "Main Menu", 6);
     document.getElementById("MainMenu_Line_1").innerText
         = "Welcome "+ user;
     document.getElementById("MainMenu_Line_2").innerText
         = "What would you like to do?";
-    document.getElementById("MainMenu_Line_3").appendChild(CreateBtn("MainMenu_Line_3", "Start a new game"));
-    document.getElementById("MainMenu_Line_4").appendChild(CreateBtn("MainMenu_Line_3", "Change my name"));
+    document.getElementById("MainMenu_Line_3").appendChild
+        (CreateBtn("MainMenu_btn_Line_3", "0 Parameters", "Prina"));
+
+    document.getElementById("MainMenu_Line_4").appendChild
+        (CreateBtn("MainMenu_btn_Line_4", "1 Parameters", "Prina","1"));
+
+    document.getElementById("MainMenu_Line_5").appendChild
+        (CreateBtn("MainMenu_btn_Line_5", "2 Parameters", "Prina","1","2"));
+
+    document.getElementById("MainMenu_Line_6").appendChild
+        (CreateBtn("MainMenu_btn_Line_6", "3 Parameters", "Prina", "1", "2","3"));
 }
 
 //creates a generic menu on the fly
--- assets/rpg/js/TagCreator.js
+++ assets/rpg/js/TagCreator.js
@@ -10,9 +10,55 @@ function CreateBr() {
     return Br;
 }
 
-function CreateBtn(id, text) {
+////CreateButtons////////CreateButtons////
+    /* Basic Button */
+    /*  function CreateBtn(id, text, func) {
+        let Btn = document.createElement("button");
+        Btn.id = id;
+        Btn.innerText = text;
+        Btn.setAttribute("onClick", func+"();");
+
+        return Btn;
+    } */
+    /* Button W/ 1 parameter */
+    /* function CreateBtn(id, text, func, args1) {
+        let Btn = document.createElement("button");
+        Btn.id = id;
+        Btn.innerText = text;
+        Btn.setAttribute("onClick", func+"('"+args1+"');");
+
+        return Btn;
+    } */
+    /* Button W/ 2 parameters */
+    /* function CreateBtn(id, text, func, args1, args2) {
+        let Btn = document.createElement("button");
+        Btn.id = id;
+        Btn.innerText = text;
+        Btn.setAttribute("onClick", func+"('"+args1+"','"+args2+"');");
+
+        return Btn;
+    } */
+
+    /* Button W/ 3 parameters */
+    /* function CreateBtn(id, text, func, args1, args2, args3) {
+        let Btn = document.createElement("button");
+        Btn.id = id;
+        Btn.innerText = text;
+        Btn.setAttribute("onClick", func+"('"+args1+"','"+args2+"','"+args3+"');");
+
+        return Btn;
+    } */
+/* Multi */
+function CreateBtn(id, text, func, args1, args2, args3) {
     let Btn = document.createElement("button");
     Btn.id = id;
     Btn.innerText = text;
+    if (args1 == "undefined" && args2 && "undefined" && args3 == "undefined"){
+        Btn.setAttribute("onClick", func + "();");
+    }else{
+        Btn.setAttribute("onClick", func + "('" + args1 + "','" + args2 + "','" + args3 + "');");
+    }
     return Btn;
 }
+
+////CreateButtons////////CreateButtons////
